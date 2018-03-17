4c056bdd0ab9adc76f5f080d7701b1a37b536d9b
eleonorakiziv <eleonora_kiziv@brown.edu>
2018-03-17 20:30:35 +0000

    Cleaning up

--- simpletest.py
+++
@@ -1,9 +0,0 @@
-# Trying to read the actuator's position
-# The north/south actuator is pin 0 on the ADC (an ADS1115),
-# and the east/west actuator is pin 1.
-
-import Adafruit_ADS1x15
-
-# Create an ADS1115 ADC (16-bit) instance.
-adc = Adafruit_ADS1x15.ADS1115()
-
