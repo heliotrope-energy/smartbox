64348c595b3afea0ee5eac8bacc9ca2d81c90905
Stephen <brawner@gmail.com>
2018-03-17 18:41:56 -0400

    Adding interface classes, some are empty for now, and the start of a command line client

---
+++ src/smartbox/sb_tracker.py
@@ -0,0 +1,196 @@
+from gpiozero import Motor, CPUTemperature
+import Adafruit_ADS1x15
+
+class SmartBoxTracker:
+	def __init__(self):
+		self.TOLERANCE = 0.1
+        self.adc = Adafruit_ADS1x15.ADS1115()
+        self.GAIN  = 2/3
+        self.EW_PIN = 1
+        self.NS_PIN = 0
+        self.scale = 6.144/32767
+
+
+        self.limits = {self.NS_PIN:[0,6.0], self.EW_PIN:[0,12.0]}
+        self.ns_motor = Motor(26,20)
+		self.ew_motor = Motor(21,16)
+        self.motors = {self.NS_PIN:self.ns_motor, self.EW_PIN:self.ew_motor}
+        self.actuator_conversions = {
+        	self.NS_PIN: [-1.335, 6.917],
+        	self.EW_PIN: [-2.67, 13.83]}
+        self.actuator_names = {self.NS_PIN: "North-South", self.EW_PIN: "East-West"}
+        self.actuator_directions = {
+        	self.NS_PIN: ["North", "South"], 
+        	self.EW_PIN: ["East", "West"]}
+
+    def get_ns_position(self):
+    	"""
+    		Returns the position in inches of the North-South Actuator
+    	"""
+    	return self.get_ns_position(self.NS_PIN)
+
+    def get_ew_position(self):
+    	"""
+    		Returns the position in inches of the East-West Actuator
+    	"""
+    	return self.get_ew_position(self.EW_PIN)
+
+    def get_ns_angle(self):
+    	"""
+    		TODO
+    		Returns the position in degrees of the North-south direction
+    	"""
+    	return 0.0
+
+    def get_ew_angle(self):
+    	"""
+    		TODO
+    		Returns the position in degrees of the East-West direction
+    	"""
+    	return 0.0
+
+    def get_ns_limits(self):
+    	return self.limits[self.NS_PIN]
+
+    def get_ew_limits(self):
+    	return self.limits[self.EW_PIN]
+
+    def is_panel_moving(self):
+    	"""
+    		TODO
+    		Returns status of 
+    	"""
+    	return self.is_ns_moving() or self.is_ew_moving()
+
+    def is_ns_moving(self):
+    	"""
+
+    	"""
+    	return self.ns_motor.value
+
+    def is_ew_moving(self):
+    	"""
+
+    	"""
+    	return self.ew_motor.value
+
+   	def move_panel_to_linear_position(self, ns_pos, ew_pos):
+   		"""
+   			Moves the panel to given linear actuator positions
+   			Params:
+   				ns_pos: The north-south position in inches from full-retraction
+   				ew_pos: The east-west position in inches from full-retraction
+   		"""
+        self._move_axis_to_linear_position_(self.EW_PIN, ew_pos)
+        self._move_axis_to_linear_position_(self.NS_PIN, ns_pos)
+
+    def move_panel_to_angular_position(self, ns_angle, ew_angle):
+    	"""
+    		TODO
+    		Moves the panel to the given North-South and East-West angles
+    		Params:
+    			ns_angle: The angle to move the panel from the north-south axis.
+    					  North is positive
+    			ew_angle: The angle to move the panel from the east-west axis.
+    					  West is positive
+    	"""
+
+    def stow(self):
+    	"""
+    		Moves the panel to a safe wind-stow position
+    	"""
+    	self.move_panel_to_linear_position(4.5, 6.0)
+
+    def move_north(self):
+    	"""
+    		Moves the panel to face north. Call stop_ns() or stop() to stop 
+    		the movement
+    	"""
+    	self._move_axis_(self.NS_PIN, True)
+
+    def move_south(self):
+    	"""
+    		Moves the panel to face south. Call stop_ns() or stop() to stop 
+    		the movement
+    	"""
+
+    	self._move_axis_(self.NS_PIN, False)
+
+    def move_east(self):
+    	"""
+    		Moves the panel to face east. Call stop_ew() or stop() to stop 
+    		the movement
+    	"""
+    	self._move_axis_(self.EW_PIN, True)
+
+    def move_west(self):
+    	"""
+    		Moves the panel to face west. Call stop_ew() or stop() to stop 
+    		the movement
+    	"""
+    	self._move_axis_(self.EW_PIN, False)
+
+    def stop_ns(self):
+    	"""
+    		Stops the movement of the north-south axis
+    	"""
+    	self._stop_axis_(self.NS_PIN)
+
+    def stop_ew(self):
+    	"""
+    		Stops the movement of the east-west axis
+    	"""
+    	self._stop_axis_(self.EW_PIN)
+
+    def stop(self):
+    	"""
+    		Stops the movement of both axes
+    	"""
+    	self.stop_ns()
+    	self.stop_ew()
+
+
+    def _actuator_voltage_to_position_(self, pin_num, voltage): 
+    	scale, offset = self.actuator_conversions[pin_num]
+    	position = voltage * scale + offset
+    	return position
+
+    def _get_position_(self, direction_pin):
+        value = self.adc.read_adc(direction_pin, gain=self.GAIN)
+        voltage = value * self.scale
+        inches =  self._actuator_voltage_to_position_(direction_pin, voltage); 
+        return inches
+
+    def _move_axis_to_linear_position_(self, direction_pin, pos):
+        min_limit, max_limit = self.limits[direction]
+        current_position = self.get_position(direction)
+        motor = self.motors[direction]
+
+        if pos < min_limit:
+            pos = min_limit
+        if pos > max_limit:
+            pos = max_limit
+
+        while abs(current_position - pos) > self.TOLERANCE: 
+            current_position = self.get_position(direction)
+            print("Current Pos {}\nDesired pos {}".format(current_position, pos))
+            
+            direction = current_position > pos
+            self._move_axis_(direction_pin, direction)
+
+        motor.stop()
+
+    def _move_axis_(self, direction_pin, forward_or_backward):
+    	actuator = self.actuators[direction]
+    	forward_dir, backward_dir = self.actuator_directions[direction_pin]
+    	if forward_or_backward:
+    		print("Moving {}".format(forward_dir))
+    		actuator.forward(speed = 1)
+    	else:
+    		print("Moving {}".format(backward_dir))
+    		actuator.backward(speed = 1)
+
+    def _stop_axis_(self, direction_pin):
+    	print("Stopping {} actuator".format(self.actuator_names[direction_pin]))
+    	self.actuators[direction].stop()
+
---
+++ src/smartbox_cli/smartbox_cli.py
@@ -0,0 +1,142 @@
+from smartbox import SmartBoxTracker
+import curses
+
+
+tracker = SmartBoxTracker()
+
+def display_help(stdscr):
+	help_message = ""
+	for key, descr in KEY_BINDINGS:
+		help_message += "{}: {}\n".format(key, descr)
+	stdscr.addstr(0, 0, help_message)
+
+def display_info(stdscr):
+	ew_position = tracker.get_ew_position()
+	ew_angle = tracker.get_ew_angle()
+	ns_position = tracker.get_sw_position()
+	ns_angle = tracker.get_ew_angle()
+
+	ns_moving = "MOVING" if tracker.is_ns_moving() else ""
+	ew_moving = "MOVING" if tracker.is_ew_moving() else ""
+
+	stdscr.addstr(15, 0, "NS position: {}\tangle: {}\t{}".format(ns_position, ns_angle, ns_moving))
+	stdscr.addstr(16, 0, "EW position: {}\tangle: {}\t{}".format(ew_position, ew_angle, ew_moving))
+
+def handle_key(stdscr, key_press):
+	stdscr.addstr(0, 70, key_press)
+	stdscr.refresh()
+	if key_press in FUNCTION_BINDINGS:
+		FUNCTION_BINDINGS[key_press](stdscr)
+
+def move_north(stdscr):
+	stdscr.addstr(0, 70, "North")
+	stdscr.refresh()
+	tracker.move_north()
+
+def move_south(stdscr):
+	stdscr.addstr(0, 70, "South")
+	stdscr.refresh()
+	tracker.move_south()
+
+def move_east(stdscr):
+	stdscr.addstr(0, 70, "East")
+	stdscr.refresh()
+	tracker.move_east()
+
+def move_west(stdscr):
+	stdscr.addstr(0, 70, "West")
+	stdscr.refresh()
+	tracker.move_west()
+
+def stop(stdscr):
+	stdscr.addstr(0, 70, "Stop")
+	stdscr.refresh()
+	tracker.stop()
+
+def get_float_input(stdscr):
+	stdscr.addstr(25, 0, " " * 80)
+	stdscr.refresh()
+	curses.echo()
+	user_input = stdscr.getstr(25, 0, 15)
+	while True:
+		try:
+			value = float(user_input)
+			curses.noecho()
+			return value
+		except ValueError:
+			stdscr.addstr(15, 0, "Invalid input, must be convertible to a numerical value")
+			stdscr.refresh()
+
+def move_to_linear_position(stdscr):
+	stdscr.addstr(24, 0, "Please type the NS linear position (0 - 6 inches)")
+	stdscr.refresh()
+	ns_pos = get_float_input(stdscr)
+
+	stdscr.addstr(24, 0, "Please type the EW linear position (0 - 12 inches)")
+	stdscr.refresh()
+	ew_pos = get_float_input(stdscr)
+	stdscr.addstr(24, 0, " " * 80)
+	stdscr.addstr(25, 0, " " * 80)
+	stdscr.addstr(5, 70, str(ns_pos))
+	stdscr.addstr(6, 70, str(ew_pos))
+	stdscr.refresh()
+	tracker.move_to_linear_position(ns_pos, ew_pos)
+
+
+def move_to_angular_position(stdscr):
+	stdscr.addstr(24, 0, "Please type the NS angular position (0 - 180)")
+	stdscr.refresh()
+	ns_pos = get_float_input(stdscr)
+
+
+	stdscr.addstr(24, 0, "Please type the EW angular position (0 - 180)")
+	stdscr.refresh()
+	ew_pos = get_float_input(stdscr)
+	stdscr.addstr(24, 0, " " * 80)
+	stdscr.addstr(25, 0, " " * 80)
+	stdscr.addstr(5, 70, str(ns_pos))
+	stdscr.addstr(6, 70, str(ew_pos))
+	stdscr.refresh()
+	tracker.move_to_angular_position(ns_pos, ew_pos)
+
+def draw_screen(stdscr):
+	stdscr.clear()
+	stdscr.erase()
+	display_help(stdscr)
+	display_info(stdscr)
+	stdscr.refresh()
+
+def main(stdscr):
+	draw_screen(stdscr)
+	key_press = stdscr.getkey()
+	while key_press != "q":
+		draw_screen(stdscr)
+		handle_key(stdscr, key_press)
+		key_press = stdscr.getkey()
+
+KEY_BINDINGS = [
+	["q", "Quit"],
+	["n or up arrow", "Move panel north"],
+	["s or down arrow", "Move panel south"],
+	["e or right arrow", "Move panel east"],
+	["w or left arrow", "Move panel west"],
+	["p", "Move panel actuators to linear position"],
+	["a", "Move panel to angular position"],
+]
+
+FUNCTION_BINDINGS = {
+	"n": move_north,
+	"s": move_south,
+	"e": move_east,
+	"w": move_west,
+	"KEY_UP": move_north,
+	"KEY_DOWN": move_south,
+	"KEY_RIGHT": move_east,
+	"KEY_LEFT": move_west,
+	"p": move_to_linear_position,
+	"a": move_to_angular_position,
+	" ": stop
+}
+
+if __name__ == "__main__":
+	curses.wrapper(main)
