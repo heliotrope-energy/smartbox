913de50a0ba2e96ae918673d9234c53e6510c35f
Stephen <brawner@gmail.com>
2018-03-19 15:13:35 -0400

    Updates

---
+++ src/smartbox/sb_camera.py
@@ -0,0 +1,11 @@
+import cv2
+
+class SmartBoxCamera:
+	def __init__(self):
+		self.cap = cv2.VideoCapture(0)
+        
+	def get_image(self):
+		ok, fr = self.cap.read()
+        fr  = cv2.flip(fr, 1) 
+        cv2image = cv2.cvtColor(fr, cv2.COLOR_BGR2RGBA)
+        return cv2image
