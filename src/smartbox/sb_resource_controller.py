81b3b93d1beab184530476253771eb88f02def10
Stephen <brawner@gmail.com>
2018-03-20 14:45:53 -0400

    Adding resource controller server and client

---
+++ src/smartbox/sb_resource_controller.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+from queue import Queue
+from concurrent import futures
+import time
+
+import grpc
+
+from smartbox.sb_tracker import SmartBoxTracker
+from smartbox.sb_charge_controller import SmartBoxChargeController
+
+import smartbox_resource_controller_pb2
+import smartbox_resource_controller_pb2_grpc
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class SmartBoxResourceController(smartbox_resource_controller_pb2_grpc.SmartBoxResourceControllerServicer):
+	accumulated_energy_at_control_start = 0.0
+	controlling_security_level = -1
+	security_level_queue = Queue()
+
+	def __init__(self):
+		self.charge_controller = SmartBoxController()
+		self.tracker_controller = SmartBoxTracker()
+		self.energy_collected_at_start = 0.0
+		self.energy_collected_at_current_time = 0.0
+		self.energy_expended_at_start = 0.0
+		self.energy_expended_at_current_time = 0.0
+
+	def get_tracker_status(self, request, context):
+		return self._get_tracker_status_message_()
+
+	def request_control(self, request, context):
+		if self.controlling_security_level == -1:
+			self.controlling_security_level = request.security_level
+			return smartbox_resource_controller_pb2.RequestControlResponse(message="Success")
+		elif request.security_level < self.controlling_security_level:
+			self.security_level_queue.put(self.controlling_security_level)
+			self.controlling_security_level = request.security_level
+			return smartbox_resource_controller_pb2.RequestControlResponse(message="Success")
+		elif request.security_level >= self.controlling_security_level:
+			return smartbox_resource_controller_pb2.RequestControlResponse(message="Failure", why_failure_id=smartbox_resource_controller_pb2.RequestControlResponse.INSUFFICIENT_SECURITY_LEVEL)
+
+	def relinquish_control(self, request, context):
+		self.controlling_security_level = -1
+		return smartbox_resource_controller_pb2.RequestControlResponse(message="Success")
+
+	def move_panel(self, request, context):
+		move_type = request.move_type
+		if move_type == smartbox_resource_controller_pb2.MoveRequest.DURATION:
+			direction = request.direction
+			if direction == smartbox_resource_controller_pb2.NORTH:
+				self.tracker_controller.move_north()
+			elif direction == smartbox_resource_controller_pb2.SOUTH:
+				self.tracker_controller.move_south()
+			elif direction == smartbox_resource_controller_pb2.EAST:
+				self.tracker_controller.move_east()
+			elif direction == smartbox_resource_controller_pb2.WEST:
+				self.tracker_controller.move_west()
+
+		elif move_type == smartbox_resource_controller_pb2.MoveRequest.POSITION:
+			ns_position = request.position.ns
+			ew_position = request.position.ew
+			self.tracker_controller.move_to_linear_position(ns_position, ew_position)
+		elif move_type == smartbox_resource_controller_pb2.MoveRequest.ANGLE:
+			ns_angle = request.angle.ns
+			ew_angle = request.angle.ew
+			self.tracker_controller.move_to_angular_position(ns_angle, ew_angle)
+
+
+
+	def stop(self, request, context):
+		self.tracker_controller.stop()
+		return smartbox_resource_controller_pb2.StopResponse(message="Success")
+
+	def stow(self, request, context):
+		self.tracker_controller.stow()
+		return smartbox_resource_controller_pb2.StowResponse(message="Success")
+
+	def _get_tracker_status_message_(self):
+		response = smartbox_resource_controller_pb2.TrackerSystemStatusResponse()
+		charge_data = self.charge_controller.get_all_data()
+
+		response.position_ns = tracker_controller.get_ns_position()
+		response.position_ew = tracker_controller.get_ew_position()
+		response.angle_ns = tracker_controller.get_ns_angle()
+		response.angle_ew = tracker_controller.get_ew_angle()
+		response.is_moving_ns = tracker_controller.is_ns_moving()
+		response.is_moving_ew = tracker_controller.is_ew_moving()
+		response.current_controlling_level = self.controlling_security_level
+
+		response.battery_voltage = charge_data[SmartBoxController.ADC_VB_F]
+		response.array_voltage = charge_data[SmartBoxController.ADC_VA_F]
+		response.load_voltage = charge_data[SmartBoxController.ADC_VL_F]
+		response.charge_current = charge_data[SmartBoxController.ADC_IC_F]
+		response.load_current = charge_data[SmartBoxController.ADC_IL_F]
+		response.charge_state = charge_data[SmartBoxController.CHARGE_STATE]
+		response.energy_collected = self.energy_collected_at_current_time - \
+			self.energy_collected_at_start
+		response.energy_expended = self.energy_expended_at_current_time	- \
+			self.energy_expended_at_start
+
+		return response
+				
+
+	
+
+def serve():
+	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+	smartbox_resource_controller_pb2_grpc.add_SmartBoxResourceControllerServicer_to_server(SmartBoxResourceController(), server)
+	server.add_insecure_port('[::]:50051')
+	server.start()
+	print("Starting resource controller server")
+	try:
+		while True:
+			time.sleep(_ONE_DAY_IN_SECONDS)
+	except KeyboardInterrupt:
+		server.stop(0)
+
+
+if __name__ == '__main__':
+	serve()
+
+
---
+++ src/smartbox/sb_resource_controller_client.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+import grpc
+
+import smartbox_resource_controller_pb2
+import smartbox_resource_controller_pb2_grpc
+
+class SmartBoxResourceControllerClient():
+	def __init__(self, security_level):
+		self.channel = grpc.insecure_channel('localhost:50051')
+		self.stub = smartbox_resource_controller_pb2_grpc.SmartBoxResourceControllerStub(self.channel)
+		self.security_level = security_level
+
+	def get_ns_position(self):
+		"""
+			Returns the position in inches of the North-South Actuator
+		"""
+		msg = self._request_status_()
+		return msg.tracker.position.ns
+
+	def get_ew_position(self):
+		"""
+			Returns the position in inches of the East-West Actuator
+		"""
+		msg = self._request_status_()
+		return msg.tracker.position.ew
+
+	def get_ns_angle(self):
+		"""
+			TODO
+			Returns the position in degrees of the North-south direction
+		"""
+		msg = self._request_status_()
+		return msg.tracker.angle.ns
+
+	def get_ew_angle(self):
+		"""
+			TODO
+			Returns the position in degrees of the East-West direction
+		"""
+		msg = self._request_status_()
+		return msg.tracker.angle.ew
+
+	def get_ns_limits(self):
+		"""
+			TODO
+		"""
+		return [0.0, 6.0]
+
+	def get_ew_limits(self):
+		"""
+			TODO
+		"""
+		return [0.0, 12.0]
+
+	def is_panel_moving(self):
+		"""
+			TODO
+			Returns status of 
+		"""
+		msg = self._request_status_()
+		return msg.tracker.move_status.ns or msg.tracker.move_status.ew
+
+	def is_ns_moving(self):
+		"""
+
+		"""
+		msg = self._request_status_()
+		return msg.tracker.move_status.ns
+
+	def is_ew_moving(self):
+		"""
+
+		"""
+		msg = self._request_status_()
+		return msg.tracker.move_status.ew
+
+	def move_panel_to_linear_position(self, ns_pos, ew_pos):
+		"""
+			Moves the panel to given linear actuator positions
+			Params:
+				ns_pos: The north-south position in inches from full-retraction
+				ew_pos: The east-west position in inches from full-retraction
+		"""
+		self._request_position_move_(ns_pos, ew_pos)
+
+	def move_panel_to_angular_position(self, ns_angle, ew_angle):
+		"""
+			TODO
+			Moves the panel to the given North-South and East-West angles
+			Params:
+				ns_angle: The angle to move the panel from the north-south axis.
+						  North is positive
+				ew_angle: The angle to move the panel from the east-west axis.
+						  West is positive
+		"""
+		self._request_angular_move_(ns_angle, ew_angle)
+
+	def stow(self):
+		"""
+			Moves the panel to a safe wind-stow position
+		"""
+		request = smartbox_resource_controller_pb2.StowRequest(message="Flat please")
+		self.stub.stow(request)
+
+	def move_north(self):
+		"""
+			Moves the panel to face north. Call stop_ns() or stop() to stop 
+			the movement
+		"""
+		self._request_direction_move_(direction=smartbox_resource_controller_pb2.MovePanelRequest.NORTH)
+		
+
+	def move_south(self):
+		"""
+			Moves the panel to face south. Call stop_ns() or stop() to stop 
+			the movement
+		"""
+
+		self._request_direction_move_(direction=smartbox_resource_controller_pb2.MovePanelRequest.SOUTH)
+		
+	def move_east(self):
+		"""
+			Moves the panel to face east. Call stop_ew() or stop() to stop 
+			the movement
+		"""
+		self._request_direction_move_(direction=smartbox_resource_controller_pb2.MovePanelRequest.EAST)
+		
+	def move_west(self):
+		"""
+			Moves the panel to face west. Call stop_ew() or stop() to stop 
+			the movement
+		"""
+		self._request_direction_move_(direction=smartbox_resource_controller_pb2.MovePanelRequest.WEST)
+
+	def stop(self):
+		"""
+			Stops the movement of both axes
+		"""
+		request = smartbox_resource_controller_pb2.StopRequest(message="stop")
+		self.stub.stop(request)
+
+	def _request_status_(self):
+		request = smartbox_resource_controller_pb2.TrackerSystemStatusRequest(message = "hello")
+		return self.stub.get_tracker_status(request)
+
+	def _request_control_(self):
+		request = smartbox_resource_controller_pb2.RequestControlRequest(message="Control please", security_level = self.security_level)
+		return self.stub.request_control(request)
+
+	def _relinquish_control_(self):
+		request = smartbox_resource_controller_pb2.RelinquishControl(message="All done")
+		return self.stub.request_control(request)
+
+	def _request_direction_move_(self, direction):
+		request = smartbox_resource_controller_pb2.MoveRequest(move_type = smartbox_resource_controller_pb2.MoveRequest.DIRECTION)
+		request.direction = direction
+		return self.stub.move_panel(request)
+
+	def _request_position_move_(self, pos_ns, pos_ew):
+		request = smartbox_resource_controller_pb2.MoveRequest(move_type = smartbox_resource_controller_pb2.MoveRequest.POSITION)
+		request.position.ns = pos_ns
+		request.position.ew = pos_ew
+		return self.stub.move_panel(request)
+
+	def _request_angular_move_(self, angle_ns, angle_ew):
+		request = smartbox_resource_controller_pb2.MoveRequest(move_type = smartbox_resource_controller_pb2.MoveRequest.ANGLE)
+		request.angle.ns = angle_ns
+		request.angle.ew = angle_ew
+		return self.stub.move_panel(request)
+
+if __name__ == "__main__":
+	client = SmartBoxResourceControllerClient(101)
+	print(client.get_ns_position())
+	print(client.get_ew_position())
+	print(client.get_ew_angle())
+	print(client.get_ew_angle())
+	print(client.is_ew_moving())
+	print(client.is_ns_moving())
+	print(client.move_panel_to_linear_position(0.0, 0.0))
+	print(client.move_panel_to_angular_position(0.0, 0.0))
+	print(client.stow())
+	print(client.stop())
+
+
+
