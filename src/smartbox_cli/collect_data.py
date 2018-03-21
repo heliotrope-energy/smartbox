1228ebaa4c2ab3e6885e542a44d2131a480cd1ed
Stephen <brawner@gmail.com>
2018-03-21 17:56:16 +0000

    updating collect data

--- src/smartbox_cli/collect_data.py
+++ src/smartbox_cli/collect_data.py
@@ -17,7 +17,7 @@ while True:
     ew_moving = tracker.is_ew_moving()
     ns_moving = tracker.is_ns_moving()
 
-    data = controller.get_all_data()
+    data, _ = controller.get_all_data()
     img = camera.get_image()
     dt = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     image_filename = dt + ".np"
