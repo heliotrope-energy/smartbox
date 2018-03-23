54b1cd45fdc444dd3cf16d6e31775dc20d911e55
Stephen <brawner@gmail.com>
2018-03-23 12:26:11 -0400

    Switching to cv2

--- src/smartbox/camera_capture.py
+++ src/smartbox/camera_capture.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, time, os, datetime
+import sys, time, os, datetime, cv2
 
 _IMAGE_FLAGS_ = "-t 1 -n -sh 25 -co 25"
 _BASE_DIRECTORY_ = "/var/www/html/images"
@@ -9,7 +9,11 @@ _TMP_FILENAME_ = "/var/www/html/images/image.png"
 _STREAM_FILENAME_ = "/var/www/html/images/most_recent.png"
 
 def capture_image(filename, save=False):
-  os.system("raspistill -o {} {}".format(_TMP_FILENAME_, _IMAGE_FLAGS_))  
+  ok, fr = self.cap.read()
+  fr  = cv2.flip(fr, 1) 
+  cv2image = cv2.cvtColor(fr, cv2.COLOR_BGR2RGBA)
+  cv2.imwrite(_TMP_FILENAME_, cv2image)
+
 
 def main():
   if not os.path.exists(_BASE_DIRECTORY_):
