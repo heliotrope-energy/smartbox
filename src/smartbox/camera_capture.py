2f3d350660a495e325dfb20a097563ce4a2885d2
Stephen <brawner@gmail.com>
2018-03-23 16:25:38 +0000

    Switching to cv2

--- src/smartbox/camera_capture.py
+++ src/smartbox/camera_capture.py
@@ -8,16 +8,16 @@ _SLEEP_TIME_ = 0.1
 _TMP_FILENAME_ = "/var/www/html/images/image.png"
 _STREAM_FILENAME_ = "/var/www/html/images/most_recent.png"
 
-def capture_image(filename, save=False):
+def capture_image():
   os.system("raspistill -o {} {}".format(_TMP_FILENAME_, _IMAGE_FLAGS_))  
 
 def main():
   if not os.path.exists(_BASE_DIRECTORY_):
     os.mkdir(_BASE_DIRECTORY_)
   
-  num_tmp_pics = max(1, int(round(_SLEEP_TIME_ / _REFRESH_TIME_)))
+  #num_tmp_pics = max(1, int(round(_SLEEP_TIME_ / _REFRESH_TIME_)))
   while True:
-    capture_image(filename, i == 0)
+    capture_image()
     time.sleep(_SLEEP_TIME_)
 
 if __name__ == "__main__":
