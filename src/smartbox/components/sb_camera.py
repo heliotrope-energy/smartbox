a9996195888681907f9c49bf1e51e5d0c34cafdb
Stephen <brawner@gmail.com>
2018-04-09 17:59:03 -0400

    Adding reconnection

--- src/smartbox/components/sb_camera.py
+++ src/smartbox/components/sb_camera.py
@@ -1,10 +1,17 @@
-import cv2
+import cv2, logging
 
 class SmartBoxCamera:
 	def __init__(self):
+		logger = logging.getLogger(__name__)
 		self.cap = cv2.VideoCapture(0)
+		if not self.cap.isOpened():
+			logger.error("Imaging camera could not be opened. Calling get_image() will try " + \
+				"to open it again, but this does not portend well.")
 		
 	def get_image(self):
+		if not selp.cap.isOpened() and not self.cap.open(0):
+			logger.error("Imaging camera could not be opened. Returning a NoneType")
+			return None
 		ok, fr = self.cap.read()
 		fr  = cv2.flip(fr, 1) 
 		cv2image = cv2.cvtColor(fr, cv2.COLOR_BGR2RGBA)
