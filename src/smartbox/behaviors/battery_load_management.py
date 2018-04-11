610ea19450704f135b192fad0a9168d85ea31f45
Stephen <brawner@gmail.com>
2018-04-11 17:32:55 -0400

    Updating client

--- src/smartbox/behaviors/battery_load_management.py
+++ src/smartbox/behaviors/battery_load_management.py
@@ -13,9 +13,10 @@ def get_tracker_data(client):
 
 def process_for_state(client, logger):
 	batt_voltage, charge_current, charge_state = get_tracker_data(client)
+	charge_state_name = tracker_pb2.Name(charge_state)
 	is_light_on = client.light.get_light_status()
 	logger.info("Battery {:.3f} Charge Current {:.3f} Charge State {} Light on? {}".format(\
-		batt_voltage, charge_current, charge_state, is_light_on))
+		batt_voltage, charge_current, charge_state_name, is_light_on))
 	
 	if charge_state == tracker_pb2.FLOAT and not is_light_on:
 		if batt_voltage > LIGHT_ON_VOLTAGE:
